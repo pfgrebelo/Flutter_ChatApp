9c2e94dd9188b9423a462600315e05184de80075
Pedro Rebelo <pfiliperebelo@gmail.com>
2023-01-18 17:59:58 +0000

    added auth form

--- lib/widgets/auth/auth_form.dart
+++ lib/widgets/auth/auth_form.dart
@@ -8,6 +8,25 @@ class AuthForm extends StatefulWidget {
 }
 
 class _AuthFormState extends State<AuthForm> {
+  final _formKey = GlobalKey<FormState>();
+  var _isLogin = true;
+  var _userEmail = '';
+  var _userName = '';
+  var _userPassword = '';
+
+  void _trySubmit() {
+    final isValid = _formKey.currentState!.validate();
+    FocusScope.of(context).unfocus(); //closes keyboard on submit
+
+    if (isValid) {
+      _formKey.currentState!.save();
+      print(_userEmail);
+      print(_userName);
+      print(_userPassword);
+      //Use those values to send our auth request ...
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -17,40 +36,73 @@ class _AuthFormState extends State<AuthForm> {
           child: Padding(
             padding: EdgeInsets.all(16),
             child: Form(
+              key: _formKey,
               child: Column(
                 mainAxisSize: MainAxisSize.min,
                 children: [
                   TextFormField(
+                    key: ValueKey('email'),
+                    validator: (value) {
+                      if (value!.isEmpty || !value.contains('@')) {
+                        return 'Please enter a valid email address.';
+                      }
+                      return null;
+                    },
                     keyboardType: TextInputType.emailAddress,
                     decoration: InputDecoration(
                       labelText: 'Email address',
                       labelStyle: TextStyle(fontWeight: FontWeight.bold),
                     ),
+                    onSaved: (newValue) {
+                      _userEmail = newValue!;
+                    },
                   ),
+                  if(!_isLogin)
                   TextFormField(
+                    key: ValueKey('username'),
+                    validator: (value) {
+                      if (value!.isEmpty || value.length < 3) {
+                        return 'Please enter at least 3 characters';
+                      }
+                      return null;
+                    },
                     decoration: InputDecoration(
                       labelText: 'Username',
                       labelStyle: TextStyle(fontWeight: FontWeight.bold),
                     ),
+                    onSaved: (newValue) {
+                      _userName = newValue!;
+                    },
                   ),
                   TextFormField(
+                    key: ValueKey('password'),
+                    validator: (value) {
+                      if (value!.isEmpty || value.length < 7) {
+                        return 'Password must be at least 7 characters long.';
+                      }
+                      return null;
+                    },
                     decoration: InputDecoration(
                       labelText: 'Password',
                       labelStyle: TextStyle(fontWeight: FontWeight.bold),
                     ),
                     obscureText: true,
+                    onSaved: (newValue) {
+                      _userPassword = newValue!;
+                    },
                   ),
-                  SizedBox(
-                    height: 12,
-                  ),
+                  SizedBox(height: 12),
                   ElevatedButton(
-                    onPressed: () {},
-                    style: ElevatedButton.styleFrom(),
-                    child: Text('Login'),
+                    onPressed: _trySubmit,
+                    child: Text(_isLogin ? 'Login' : 'Sign Up'),
                   ),
                   TextButton(
-                    onPressed: () {},
-                    child: Text('Create new account'),
+                    onPressed: () {
+                      setState(() {
+                        _isLogin = !_isLogin;
+                      });
+                    },
+                    child: Text(_isLogin ? 'Create new account' : 'I already have an account'),
                   )
                 ],
               ),
